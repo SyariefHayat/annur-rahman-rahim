88f62b0fdacce58c2b7e054d9c6db29c8a331eb0
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-03-25 17:19:29 +0700

    FIX: leak

--- frontend/src/services/firebase/firebase.js
+++ frontend/src/services/firebase/firebase.js
@@ -8,13 +8,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD9keYXeQMQbs9OqfZLSBQd9yC1v_en830",
-    authDomain: "first-project-7d5cd.firebaseapp.com",
-    projectId: "first-project-7d5cd",
-    storageBucket: "first-project-7d5cd.firebasestorage.app",
-    messagingSenderId: "510745042677",
-    appId: "1:510745042677:web:1918f5a552b7f9329aafbc",
-    measurementId: "G-1JCMGFLHJN"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
   };
 
 // Initialize Firebase
