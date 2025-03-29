5204f8721834f24f56472e287a5bac30875d489b
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-03-29 16:37:22 +0700

    UPDATE: Memperbarui Header dari Article Pages

--- frontend/src/pages/Article/index.jsx
+++ frontend/src/pages/Article/index.jsx
@@ -4,6 +4,8 @@ import Navbar from '../Landing/Navbar'
 import useDataArticles from '@/hooks/useDataArticles'
 import EachUtils from '@/utils/EachUtils'
 import { FaAnglesRight } from 'react-icons/fa6'
+import Footer from '@/components/Modules/Landing/Footer'
+import { LIST_CAMPAIGN } from '@/constants/listCampaign'
 
 const Article = () => {
     const { dataArticles } = useDataArticles();
@@ -11,29 +13,49 @@ const Article = () => {
     return (
         <DefaultLayout>
             <Navbar/>
-            <div className="w-full h-full">
-                {dataArticles && (
-                    <EachUtils 
-                        of={dataArticles}
-                        render={(item, index) => (
-                            <div key={index} className="w-full h-full p-5 mb-5 flex gap-5 border border-gray-300 rounded-md shadow-md">
-                                <div className="w-[20%] h-52 bg-gray-300 rounded-md overflow-hidden">
-                                    <img src={`http://localhost:8000${item.image}`} alt="" className="w-full h-full"/>
-                                </div>
-                                <div className="w-[75%] h-52 flex flex-col justify-between">
-                                    <h3 className="text-2xl font-bold">{item.title}</h3>
-                                    <p>20 Februari 2025</p>
-                                    <p>{item.content}</p>
-                                    <a href={`/article/${item._id}`} className="flex items-center gap-2">
-                                        <p>Read More</p>
-                                        <FaAnglesRight />
-                                    </a>
-                                </div>
-                            </div>
-                        )}
+            <section className="relative w-full h-screen flex items-center justify-center">
+                <div
+                    aria-hidden="true"
+                    className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+                >
+                    <div
+                        style={{
+                            clipPath:
+                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                        }}
+                        className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#4f46e5] to-[#3b82f6] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
                     />
-                )}
-            </div>
+                </div>
+
+                <div className="mx-auto max-w-3xl mt-20 px-3 sm:px-0 text-center">
+                    <h1 className="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">Selamat Datang di Blog Yatallatop</h1>
+
+                    <p className="mt-8 text-lg font-medium text-gray-500 sm:text-xl">Temukan cerita inspiratif, pembaruan aktivitas yayasan, artikel edukatif, dan banyak lagi di sini.</p>
+                </div>
+
+                <div
+                aria-hidden="true"
+                className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            >
+                <div
+                    style={{
+                        clipPath:
+                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                    }}
+                    className="relative left-[calc(50%+3rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#4f46e5] to-[#3b82f6] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                />
+                </div>
+            </section>
+
+            <section className="relative">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-300 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                        
+                    </div>
+                </div>
+            </section>
+
+            <Footer />
         </DefaultLayout>
     )
 }
