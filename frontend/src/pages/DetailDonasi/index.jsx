10cdfe5e51fdb5a57f99de03cf4820402f4643d8
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-03-29 16:29:13 +0700

    UPDATE: Menambahkan fitur tabs pada Detail Donasi Pages

--- frontend/src/pages/DetailDonasi/index.jsx
+++ frontend/src/pages/DetailDonasi/index.jsx
@@ -1,11 +1,14 @@
-import DefaultLayout from '@/components/Layouts/DefaultLayout'
 import React, { useState } from 'react'
-import Navbar from '../Landing/Navbar'
 import { useParams } from 'react-router-dom'
-import Footer from '@/components/Modules/Landing/Footer'
-import { LIST_CAMPAIGN } from '@/constants/listCampaign'
+
+import Navbar from '../Landing/Navbar'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import Footer from '@/components/Modules/Landing/Footer'
+import { LIST_CAMPAIGN } from '@/constants/listCampaign'
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 const DetailDonasi = () => {
     const { id } = useParams();
@@ -29,7 +32,7 @@ const DetailDonasi = () => {
     return (
         <DefaultLayout>
             <Navbar position="relative" />
-            <section className="overflow-hidden py-14 sm:py-24">
+            <section className="overflow-hidden py-14 sm:py-24 px-6">
                 <div className="mx-auto max-w-7xl">
                     <article className="mx-auto max-w-2xl grid grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                         <figure>
@@ -82,6 +85,128 @@ const DetailDonasi = () => {
                             </div>
                         </section>
                     </article>
+
+                    <Tabs defaultValue="campaign-story" className="w-full my-14">
+                        <TabsList className="gap-4">
+                            <TabsTrigger value="campaign-story">Cerita</TabsTrigger>
+                            <TabsTrigger value="campaign-donation">Donasi</TabsTrigger>
+                            <TabsTrigger value="campaign-pray">Doa Orang Baik</TabsTrigger>
+                        </TabsList>
+
+                        <TabsContent value="campaign-story" className="p-6 bg-white shadow-md rounded-lg mt-4">
+                            <h2 className="text-xl font-semibold mb-2">Cerita Penggalangan Dana</h2>
+                            <p>{campaign.description}</p>
+                        </TabsContent>
+
+                        <TabsContent value="campaign-donation" className="mt-4 grid grid-cols-1 lg:grid-cols-3 gap-5">
+                            <div className="flex items-center gap-x-4 shadow-md rounded-lg p-5 py-5">
+                                <Avatar className="w-14 h-14">
+                                    <AvatarImage src="https://github.com/shadcn.png" />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+
+                                <div className="text-sm/6">
+                                    <p className="font-semibold text-gray-900">
+                                        Orang Baik
+                                    </p>
+                                    <p>
+                                        Berdonasi sebesar <span className="font-semibold text-gray-900">Rp 10.000</span>
+                                    </p>
+                                    <p className="text-gray-600">10 menit yang lalu</p>
+                                </div>
+                            </div>
+
+                            <div className="flex items-center gap-x-4 shadow-md rounded-lg p-5 py-5">
+                                <Avatar className="w-14 h-14">
+                                    <AvatarImage src="https://github.com/shadcn.png" />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+
+                                <div className="text-sm/6">
+                                    <p className="font-semibold text-gray-900">
+                                        Orang Baik
+                                    </p>
+                                    <p>
+                                        Berdonasi sebesar <span className="font-semibold text-gray-900">Rp 5.000</span>
+                                    </p>
+                                    <p className="text-gray-600">1 jam yang lalu</p>
+                                </div>
+                            </div>
+
+                            <div className="flex items-center gap-x-4 shadow-md rounded-lg p-5 py-5">
+                                <Avatar className="w-14 h-14">
+                                    <AvatarImage src="https://github.com/shadcn.png" />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+
+                                <div className="text-sm/6">
+                                    <p className="font-semibold text-gray-900">
+                                        Orang Baik
+                                    </p>
+                                    <p>
+                                        Berdonasi sebesar <span className="font-semibold text-gray-900">Rp 20.000</span>
+                                    </p>
+                                    <p className="text-gray-600">1 hari yang lalu</p>
+                                </div>
+                            </div>
+                        </TabsContent>
+
+                        <TabsContent value="campaign-pray" className="mt-4 grid grid-cols-1 lg:grid-cols-3 gap-5">
+                            <div className="shadow-md rounded-lg p-5 py-5">
+                                <div className="flex items-center gap-x-4">
+                                    <Avatar className="w-10 h-10">
+                                        <AvatarImage src="https://github.com/shadcn.png" />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+
+                                    <div className="text-sm/6">
+                                        <p className="font-semibold text-gray-900">
+                                            Orang Baik
+                                        </p>
+                                        <p className="text-gray-600">1 hari yang lalu</p>
+                                    </div>
+                                </div>
+
+                                <p className="mt-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error dignissimos numquam repellat reprehenderit reiciendis corporis, delectus ducimus eum aspernatur sed nobis illo repudiandae excepturi tempore debitis pariatur. Eaque, quae exercitationem.</p>
+                            </div>
+
+                            <div className="shadow-md rounded-lg p-5 py-5">
+                                <div className="flex items-center gap-x-4">
+                                    <Avatar className="w-10 h-10">
+                                        <AvatarImage src="https://github.com/shadcn.png" />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+
+                                    <div className="text-sm/6">
+                                        <p className="font-semibold text-gray-900">
+                                            Orang Baik
+                                        </p>
+                                        <p className="text-gray-600">1 hari yang lalu</p>
+                                    </div>
+                                </div>
+
+                                <p className="mt-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error dignissimos numquam repellat reprehenderit reiciendis corporis, delectus ducimus eum aspernatur sed nobis illo repudiandae excepturi tempore debitis pariatur. Eaque, quae exercitationem.</p>
+                            </div>
+
+                            <div className="shadow-md rounded-lg p-5 py-5">
+                                <div className="flex items-center gap-x-4">
+                                    <Avatar className="w-10 h-10">
+                                        <AvatarImage src="https://github.com/shadcn.png" />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+
+                                    <div className="text-sm/6">
+                                        <p className="font-semibold text-gray-900">
+                                            Orang Baik
+                                        </p>
+                                        <p className="text-gray-600">1 hari yang lalu</p>
+                                    </div>
+                                </div>
+
+                                <p className="mt-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error dignissimos numquam repellat reprehenderit reiciendis corporis, delectus ducimus eum aspernatur sed nobis illo repudiandae excepturi tempore debitis pariatur. Eaque, quae exercitationem.</p>
+                            </div>
+                        </TabsContent>
+                    </Tabs>
                 </div>
             </section>
             <Footer />
