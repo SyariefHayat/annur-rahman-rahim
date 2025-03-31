46aac7f1f33c308b5f0cb5c317fa1cfc8a0942d7
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-03-31 23:30:55 +0700

    UPDATE: Memperbarui UI dari Contact Pages

--- frontend/src/pages/Contact/index.jsx
+++ frontend/src/pages/Contact/index.jsx
@@ -5,44 +5,166 @@ import GoogleMapComponent from '@/components/Modules/Element/GoogleMapComponents
 import Footer from '@/components/Modules/Landing/Footer'
 import EachUtils from '@/utils/EachUtils'
 import { LIST_CONTACT } from '@/constants/listContact'
+import { ChevronDown } from 'lucide-react'
+import { Label } from '@/components/ui/label'
 
 const Contact = () => {
     return (
         <DefaultLayout>
-            <Navbar/>
-            <section className="w-full h-80 bg-gradient-to-r from-sky-500 to-sky-800 mb-10 rounded-tl-[100px] rounded-br-[100px] flex flex-col items-center justify-center gap-3 text-white">
-                <h2 className="text-6xl font-bold">Kontak Kami</h2>
-            </section>
-            <div className="w-full h-screen flex gap-5">
-                <div className="w-full h-full p-5 flex flex-col gap-5">
-                    <p className="text-4xl text-blue-500 font-bold">Infomasi selengkapnya:</p>
-                    <EachUtils 
-                        of={LIST_CONTACT}
-                        render={(item, index) => (
-                            <div key={index} className="w-full p-3 flex items-center gap-3 bg-gray-300 rounded-md">
-                                <div className="w-14 h-14 flex items-center justify-center rounded-full bg-white">
-                                    <item.icon size={30} className="text-sky-500" />
-                                </div>
-                                <div>
-                                    <p className="text-2xl font-medium text-white mb-2">{item.title}</p>
-                                    <p>{item.desc}</p>
-                                </div>
-                            </div>
-                        )}
+            <Navbar />
+            <section className="relative w-full h-screen flex items-center justify-center">
+                <div
+                    aria-hidden="true"
+                    className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+                >
+                    <div
+                        style={{
+                            clipPath:
+                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                        }}
+                        className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#4f46e5] to-[#3b82f6] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
                     />
                 </div>
-                <div className="w-full h-full p-5">
-                    <p className="text-4xl text-blue-500 font-bold">Kirim pesan kamu</p>
-                    <form action="" className="w-full h-full py-5">
-                        <input type="text" className="w-full p-5 bg-gray-300 rounded-md" placeholder="Name" />
-                        <input type="text" className="w-full p-5 bg-gray-300 my-5 rounded-md" placeholder="Phone Number" />
-                        <input type="email" className="w-full p-5 bg-gray-300 rounded-md" placeholder="Email" />
-                        <textarea name="" id="" cols={63} rows={5} placeholder="Message" className="bg-gray-300 rounded-md p-5 mt-5"></textarea>
-                    </form>
+
+                <div className="mx-auto max-w-3xl mt-20 px-3 sm:px-0 text-center">
+                    <h1 className="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">Hubungi Yayasan Yatallatop</h1>
+
+                    <p className="mt-8 text-lg font-medium text-gray-500 sm:text-xl">Kami percaya bahwa setiap kontribusi, besar atau kecil, dapat membawa perubahan positif. Jangan ragu untuk menghubungi kami.</p>
                 </div>
+
+                <div
+                aria-hidden="true"
+                className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            >
+                <div
+                    style={{
+                        clipPath:
+                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                    }}
+                    className="relative left-[calc(50%+3rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#4f46e5] to-[#3b82f6] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                />
+                </div>
+            </section>
+
+            <div className="isolate px-6 py-24 sm:py-32 lg:px-8">
+                <div className="mx-auto max-w-2xl text-center">
+                    <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">Kontak Kami</h2>
+                    <p className="mt-2 text-lg/8 text-gray-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
+                </div>
+
+                <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+                    <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                        <div>
+                            <Label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">Nama Depan</Label>
+                            <div className="mt-2.5">
+                                <input
+                                    id="first-name"
+                                    name="first-name"
+                                    type="text"
+                                    autoComplete="given-name"
+                                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <Label htmlFor="last-name" className="block text-sm/6 font-semibold text-gray-900">Nama Belakang</Label>
+                            <div className="mt-2.5">
+                                <input
+                                    id="last-name"
+                                    name="last-name"
+                                    type="text"
+                                    autoComplete="family-name"
+                                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="sm:col-span-2">
+                            <Label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">Email</Label>
+                            <div className="mt-2.5">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="sm:col-span-2">
+                            <Label htmlFor="subject" className="block text-sm/6 font-semibold text-gray-900">Subjek</Label>
+                            <div className="mt-2.5">
+                                <input
+                                    id="subject"
+                                    name="subject"
+                                    type="text"
+                                    autoComplete="organization"
+                                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="sm:col-span-2">
+                            <Label htmlFor="phone-number" className="block text-sm/6 font-semibold text-gray-900">No Hp</Label>
+                            <div className="mt-2.5">
+                                <div className="flex rounded-md bg-white outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
+                                    <div className="grid shrink-0 grid-cols-1 focus-within:relative">
+                                    <select
+                                        id="country"
+                                        name="country"
+                                        autoComplete="country"
+                                        aria-label="Country"
+                                        className="col-start-1 row-start-1 w-full appearance-none rounded-md py-2 pr-7 pl-3.5 text-base text-gray-500 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    >
+                                        <option>US</option>
+                                        <option>CA</option>
+                                        <option>EU</option>
+                                    </select>
+                                    <ChevronDown
+                                        aria-hidden="true"
+                                        className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+                                    />
+                                    </div>
+                                    <input
+                                    id="phone-number"
+                                    name="phone-number"
+                                    type="text"
+                                    placeholder="123-456-7890"
+                                    className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="sm:col-span-2">
+                            <Label htmlFor="message" className="block text-sm/6 font-semibold text-gray-900">Pesan</Label>
+                            <div className="mt-2.5">
+                                <textarea
+                                    id="message"
+                                    name="message"
+                                    rows={4}
+                                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                                    defaultValue={''}
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="mt-10">
+                        <button
+                            type="submit"
+                            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Let's talk
+                        </button>
+                    </div>
+
+                </form>
             </div>
-            <GoogleMapComponent/>
-            <Footer style={"mt-10"} />
+
+            <Footer />
         </DefaultLayout>
     )
 }
