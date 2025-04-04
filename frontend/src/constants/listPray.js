d843b394225352955177e564b6ccf18edaac5f01
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-04-04 15:09:15 +0700

    UPDATE DETAIL DONASI: Merubah element div dengan card pada TabsContent

--- frontend/src/constants/listPray.js
+++ frontend/src/constants/listPray.js
@@ -1,12 +1,12 @@
 export const LIST_PRAY = [
     {
         name: "Orang Baik",
-        time: "1 hari yang lalu",
+        time: "30 menit yang lalu",
         pray: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, beatae."
     },
     {
         name: "Orang Baik",
-        time: "1 hari yang lalu",
+        time: "2 jam yang lalu",
         pray: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, beatae."
     },
     {
--- frontend/src/pages/DetailDonasi/index.jsx
+++ frontend/src/pages/DetailDonasi/index.jsx
@@ -1,5 +1,5 @@
 import { z } from "zod"
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from "react-hook-form"
 import { useParams } from 'react-router-dom'
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -22,6 +22,15 @@ import {
     DialogTrigger,
 } from "@/components/ui/dialog"
 
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+
 import Navbar from '../Landing/Navbar'
 import EachUtils from "@/utils/EachUtils"
 import { Input } from "@/components/ui/input"
@@ -36,6 +45,9 @@ import { LIST_CAMPAIGN } from '@/constants/listCampaign'
 import DefaultLayout from '@/components/Layouts/DefaultLayout'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Separator } from "@/components/ui/separator"
+import { Heart } from "lucide-react"
+import { Toggle } from "@/components/ui/toggle"
 
 const FormSchema = z.object({
     fullName: z.string()
@@ -68,6 +80,23 @@ const DetailDonasi = () => {
     const { id } = useParams();
     const index = parseInt(id, 10);
     const campaign = LIST_CAMPAIGN[index];
+
+    const [prayers, setPrayers] = useState([10, 17, 4]);
+    const [isPrays, setIsPrays] = useState([false, false, false]);
+
+    const handleTogglePray = (index) => {
+        setPrayers((prev) => {
+            const updated = [...prev];
+            updated[index] = isPrays[index] ? updated[index] - 1 : updated[index] + 1;
+            return updated;
+        });
+
+        setIsPrays((prev) => {
+            const updated = [...prev];
+            updated[index] = !prev[index];
+            return updated;
+        })
+    }
 
     const form = useForm({
         resolver: zodResolver(FormSchema),
@@ -224,42 +253,22 @@ isAnonim: ${data.isAnonymous}
                             <TabsTrigger value="campaign-pray">Doa Orang Baik</TabsTrigger>
                         </TabsList>
 
-                        <TabsContent value="campaign-story" className="p-6 bg-muted shadow-md rounded-lg mt-4">
-                            <h2 className="text-xl font-semibold mb-2">Cerita Penggalangan Dana</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed itaque et nam est qui explicabo adipisci tempora nulla ullam a iste pariatur laboriosam, esse quidem. Facere, quia eligendi? Dolores, voluptatum! Incidunt eveniet consequuntur sint itaque vel necessitatibus officia doloremque aliquam maxime in tempora optio similique fugit odit reprehenderit esse quae quo quas iste praesentium qui, pariatur ut totam? Minima, beatae. Dolorem labore fugiat minus sint, quidem minima beatae voluptas, hic cum, neque officia corrupti. Dolores maxime corrupti, tenetur quas reiciendis rerum expedita! Ut unde amet molestias deserunt? Natus laboriosam a dicta alias quasi assumenda at et molestias possimus, ipsum explicabo?</p>
+                        <TabsContent value="campaign-story" className="mt-4">
+                            <Card>
+                                <CardContent>
+                                    <h2 className="text-xl font-semibold mb-6">Cerita Penggalangan Dana</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed itaque et nam est qui explicabo adipisci tempora nulla ullam a iste pariatur laboriosam, esse quidem. Facere, quia eligendi? Dolores, voluptatum! Incidunt eveniet consequuntur sint itaque vel necessitatibus officia doloremque aliquam maxime in tempora optio similique fugit odit reprehenderit esse quae quo quas iste praesentium qui, pariatur ut totam? Minima, beatae. Dolorem labore fugiat minus sint, quidem minima beatae voluptas, hic cum, neque officia corrupti. Dolores maxime corrupti, tenetur quas reiciendis rerum expedita! Ut unde amet molestias deserunt? Natus laboriosam a dicta alias quasi assumenda at et molestias possimus, ipsum explicabo?</p>
+                                </CardContent>
+                            </Card>
                         </TabsContent>
 
                         <TabsContent value="campaign-donation" className="mt-4 grid grid-cols-1 lg:grid-cols-3 gap-5">
                             <EachUtils
                                 of={LIST_DONATUR}
                                 render={(item, index) => (
-                                    <div key={index} className="flex items-center gap-x-4 shadow-md rounded-lg p-5 py-5 bg-muted">
-                                        <Avatar className="w-14 h-14">
-                                            <AvatarImage src="https://github.com/shadcn.png" />
-                                            <AvatarFallback>CN</AvatarFallback>
-                                        </Avatar>
-
-                                        <div className="text-sm/6">
-                                            <p className="font-semibold text-gray-900">
-                                                {item.name}
-                                            </p>
-                                            <p>
-                                                Berdonasi sebesar <span className="font-semibold text-gray-900">{item.amount}</span>
-                                            </p>
-                                            <p className="text-xs/6 text-gray-600">{item.time}</p>
-                                        </div>
-                                    </div>
-                                )}
-                            />
-                        </TabsContent>
-
-                        <TabsContent value="campaign-pray" className="mt-4 grid grid-cols-1 lg:grid-cols-3 gap-5">
-                            <EachUtils
-                                of={LIST_PRAY}
-                                render={(item, index) => (
-                                    <div className="shadow-md rounded-lg p-5 py-5 bg-muted">
-                                        <div className="flex items-center gap-x-4">
-                                            <Avatar className="w-10 h-10">
+                                    <Card key={index}>
+                                        <CardContent className="flex items-center gap-x-4">
+                                            <Avatar className="w-14 h-14">
                                                 <AvatarImage src="https://github.com/shadcn.png" />
                                                 <AvatarFallback>CN</AvatarFallback>
                                             </Avatar>
@@ -268,12 +277,52 @@ isAnonim: ${data.isAnonymous}
                                                 <p className="font-semibold text-gray-900">
                                                     {item.name}
                                                 </p>
-                                                <p className="text-gray-600">{item.time}</p>
+                                                <p>
+                                                    Berdonasi sebesar <span className="font-semibold text-gray-900">{item.amount}</span>
+                                                </p>
+                                                <p className="text-xs/6 text-gray-600">{item.time}</p>
                                             </div>
-                                        </div>
+                                        </CardContent>
+                                    </Card>
+                                    
+                                )}
+                            />
+                        </TabsContent>
 
-                                        <p className="mt-5">{item.pray}</p>
-                                    </div>
+                        <TabsContent value="campaign-pray" className="mt-4 grid grid-cols-1 lg:grid-cols-3 gap-5">
+                            <EachUtils
+                                of={LIST_PRAY}
+                                render={(item, index) => (
+                                    <Card key={index} className="gap-2 pb-0">
+                                        <CardHeader>
+                                            <div className="flex items-center gap-x-4">
+                                                <Avatar className="w-10 h-10">
+                                                    <AvatarImage src="https://github.com/shadcn.png" />
+                                                    <AvatarFallback>CN</AvatarFallback>
+                                                </Avatar>
+
+                                                <div className="text-sm/6">
+                                                    <p className="font-semibold text-gray-900">
+                                                        {item.name}
+                                                    </p>
+                                                    <p className="text-gray-600">{item.time}</p>
+                                                </div>
+                                            </div>
+                                        </CardHeader>
+                                        <CardContent>
+                                            <div className="flex flex-col">
+                                                <p className="text-gray-600">{item.pray}</p>
+
+                                                <p className=" text-sm/6 mt-5 text-gray-600"><span className="text-gray-900 font-semibold">{prayers[index]} Orang</span> mengaminkan doa ini</p>
+                                            </div>
+                                        </CardContent>
+                                        <Separator className="mt-2"/>
+                                        <CardFooter className="mb-3 mt-1">
+                                            <Toggle aria-label="Pray" pressed={isPrays[index]} onPressedChange={() => handleTogglePray(index)} className="mx-auto cursor-pointer">
+                                                <Heart className={`${isPrays[index] ? "text-red-400" : ""}`}/> Amin
+                                            </Toggle>
+                                        </CardFooter>
+                                    </Card>
                                 )}
                             />
                         </TabsContent>
