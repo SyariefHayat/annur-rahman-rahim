c9599e7626a3ada13f333258e781617c7c88bbd7
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-04-19 22:08:42 +0700

    feat: menambahkan fitur skeleton pada profile postingan

--- frontend/src/components/Modules/Profile/Post/index.jsx
+++ frontend/src/components/Modules/Profile/Post/index.jsx
@@ -21,13 +21,14 @@ import {
 
 import EachUtils from '@/utils/EachUtils';
 import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
 import { userAtomStorage } from '@/jotai/atoms';
 import { getRelativeTime } from '@/utils/formatDate';
 import { apiInstanceExpress } from '@/services/express/apiInstance';
 
 const Post = () => {
     const [articles, setArticles] = useState([]);
-
+    const [isLoading, setIsLoading] = useState(true);
     const [user] = useAtom(userAtomStorage);
 
     useEffect(() => {
@@ -39,10 +40,16 @@ const Post = () => {
                     }
                 })
 
-                if (response.status === 200) return setArticles(response.data.data);
+                if (response.status === 200) {
+                    setArticles(response.data.data);
+                }
             } catch (error) {
                 console.error(error);
-            };
+            } finally {
+                setTimeout(() => {
+                    setIsLoading(false);
+                }, 5000);
+            }
         };
 
         getArticles();
@@ -67,64 +74,83 @@ const Post = () => {
             toast.error("Gagal menghapus artikel. Silakan coba lagi.", { id: toastId });
         }
     };
-    
 
     return (
         <div className="grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            <EachUtils
-                of={articles}
-                render={(item, index) => (
+            {isLoading ? (
+                Array.from({ length: 3 }).map((_, index) => (
                     <article key={index} className="flex max-w-xl h-[300px] flex-col items-start justify-between overflow-hidden">
-                        <figure className="w-full h-[70%] rounded-xl overflow-hidden">
-                            <img 
-                                src={`${import.meta.env.VITE_BASE_URL_EXPRESS}${item.cover}`} 
-                                alt={item.title} 
-                                className="w-full h-full object-cover object-center"
-                            />
-                        </figure>
+                        <Skeleton className="w-full h-[70%] rounded-xl" />
                         <div className="w-full h-[30%] flex items-start justify-between pt-4">
-                            <a href={item.href}>
-                                <h3 className="text-lg/6 font-semibold">{item.title}</h3>
-                            </a>
-
-                            <DropdownMenu>
-                                <DropdownMenuTrigger>
-                                    <Button variant="ghost" size="icon" className="size-6 rounded-sm cursor-pointer">
-                                        <EllipsisVertical />
-                                    </Button>
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent>
-                                    <DropdownMenuLabel>Pengaturan</DropdownMenuLabel>
-                                    <DropdownMenuSeparator />
-                                    <DropdownMenuItem asChild>
-                                        <Link to={`/article/${item._id}`}>Lihat</Link>
-                                    </DropdownMenuItem>
-                                    <DropdownMenuItem onClick={() => handleDelete(item._id)}>Hapus</DropdownMenuItem>
-                                </DropdownMenuContent>
-                            </DropdownMenu>
+                            <Skeleton className="w-3/4 h-6" />
+                            <Skeleton className="size-6 rounded-sm" />
                         </div>
                         <footer className="w-full flex items-center justify-between text-sm/6 text-gray-600">
                             <div className="flex gap-3">
-                                <Button variant="ghost">
-                                    <ThumbsUp/>
-                                    {item.likes}
-                                </Button>
-                                <Button variant="ghost">
-                                    <MessageCircle/>
-                                    {item.comments.length}
-                                </Button>
-                                <Button variant="ghost">
-                                    <Share2/>
-                                    {item.shares}
-                                </Button>
+                                <Skeleton className="w-10 h-6" />
+                                <Skeleton className="w-10 h-6" />
+                                <Skeleton className="w-10 h-6" />
                             </div>
-                            <p>{getRelativeTime(item.createdAt)}</p>
+                            <Skeleton className="w-16 h-4" />
                         </footer>
                     </article>
-                )}
-            />
-        </div>
-    )
-}
+                ))
+            ) : (
+                <EachUtils
+                    of={articles}
+                    render={(item, index) => (
+                        <article key={index} className="flex max-w-xl h-[300px] flex-col items-start justify-between overflow-hidden">
+                            <figure className="w-full h-[70%] rounded-xl overflow-hidden">
+                                <img 
+                                    src={`${import.meta.env.VITE_BASE_URL_EXPRESS}${item.cover}`} 
+                                    alt={item.title} 
+                                    className="w-full h-full object-cover object-center"
+                                />
+                            </figure>
+                            <div className="w-full h-[30%] flex items-start justify-between pt-4">
+                                <a href={item.href}>
+                                    <h3 className="text-lg/6 font-semibold">{item.title}</h3>
+                                </a>
 
-export default Post
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger>
+                                        <Button variant="ghost" size="icon" className="size-6 rounded-sm cursor-pointer">
+                                            <EllipsisVertical />
+                                        </Button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent>
+                                        <DropdownMenuLabel>Pengaturan</DropdownMenuLabel>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem asChild>
+                                            <Link to={`/article/${item._id}`}>Lihat</Link>
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem onClick={() => handleDelete(item._id)}>Hapus</DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </div>
+                            <footer className="w-full flex items-center justify-between text-sm/6 text-gray-600">
+                                <div className="flex gap-3">
+                                    <Button variant="ghost">
+                                        <ThumbsUp/>
+                                        {item.likes}
+                                    </Button>
+                                    <Button variant="ghost">
+                                        <MessageCircle/>
+                                        {item.comments.length}
+                                    </Button>
+                                    <Button variant="ghost">
+                                        <Share2/>
+                                        {item.shares}
+                                    </Button>
+                                </div>
+                                <p>{getRelativeTime(item.createdAt)}</p>
+                            </footer>
+                        </article>
+                    )}
+                />
+            )}
+        </div>
+    );
+};
+
+export default Post;
